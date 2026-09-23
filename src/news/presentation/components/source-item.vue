ddde9d2e43af02027113e2d4e917a8d5af22c9e6
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-23 09:16:56 -0500

    feat(news): fix source item

--- src/news/presentation/components/source-item.vue
+++ src/news/presentation/components/source-item.vue
@@ -1,27 +1,54 @@
 <script setup lang="js">
-import ArticleItem from "./article-item.vue";
-import {Article} from "@/news/domain/model/article.entity.js";
+  import {Source} from "@/news/domain/model/source.entity.js";
 
-/**
- * Presentation component for rendering a collection of article cards.
- *
- * @remarks
- * Iterates over an array of Article entities and renders an ArticleItem for each.
- */
+  /**
+   * Presentation component for a single news source item.
+   *
+   * @remarks
+   * Displays source details and emits a selection event when clicked.
+   */
 
-/**
- * Properties for the ArticleList component.
- *
- * @typedef {Object} ArticleListProps
- * @property {Article[]} articles - An array of Article entities to be displayed.
- */
-const { articles } = defineProps({ articles: { type: Array, required: true } });
+  /**
+   * Properties for the SourceItem component.
+   *
+   * @typedef {Object} SourceItemProps
+   * @property {Source} source - The source entity to display.
+   */
 
+  /**
+   * Emitted events for the SourceItem component.
+   *
+   * @typedef {Object} SourceItemEmits
+   * @property {(event: 'source-selected', source: Source) => void} source-selected - Emitted when the source is clicked.
+   */
+
+  /** @type {SourceItemProps} */
+  const { source } = defineProps({ source: { type: Source, required: true } });
+  /** @type {SourceItemEmits['emit']} */
+  const emit  = defineEmits(['source-selected']);
+
+  /**
+   * Emits a selected source to the parent component.
+   *
+   * @returns {void}
+   */
+  const emitSourceSelectedEvent = () => {
+    emit('source-selected', source);
+  };
 </script>
 
 <template>
-  <div v-for="article in articles" :key="article.url.toString()">
-    <article-item :article="article"/>
+  <div class="m-4">
+    <div @click="emitSourceSelectedEvent" class="flex align-content-start flex-wrap cursor-pointer hover:bg-emphasis p-2 border-round transition-colors transition-duration-150">
+    <span  class="flex align-items-center justify-content-center mr-2">
+          <pv-avatar :aria-label="source.name"
+                     :image="source.urlToLogo"
+                     shape="circle"/>
+    </span>
+      <span  class="flex align-items-center justify-content-center font-medium">
+          {{source.name}}
+    </span>
+    </div>
   </div>
 </template>
 
