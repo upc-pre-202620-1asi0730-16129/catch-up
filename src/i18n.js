3ee75c1e5340b25a8efb171f492d12129ce7d391
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-23 09:25:30 -0500

    chore(main): add comments

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,16 @@
-import en from './locales/en.json';
-import es from './locales/es.json';
+import en from "./locales/en.json";
+import es from "./locales/es.json";
 
 import {createI18n} from "vue-i18n";
 
+/**
+ * Shared internationalization service used across presentation modules.
+ */
 const i18n = createI18n({
     legacy: false,
-    locale: 'en',
-    fallbackLocale: 'en',
-    messages: { en, es }
+    locale: "en",
+    fallbackLocale: "en",
+    messages: {en, es}
 });
 
 export default i18n;
